01b528f403149ad23902c90dbfa1794bc59394b1
HudsonSalis <67973521+HudsonSalis@users.noreply.github.com>
2022-01-25 11:42:46 -0300

    Finalizando

---
+++ url-shortener-dio-main/src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:3000',
+	MONGO_CONNECTION:
+	'mongodb+srv://admin1:admin1@urlshortner.2jsft.mongodb.net/urlshortner?retryWrites=true&w=majority'
+}
